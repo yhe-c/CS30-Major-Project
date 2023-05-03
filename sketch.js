f313d028acb5dab987fb7ae0bac6bc8a5acd5978
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-05-02 20:37:29 -0600

    playing around with gravity, exploring p5play

--- sketch.js
+++ sketch.js
@@ -32,6 +32,7 @@
 
 //initiating global variables
 let lvl_background;
+let character_img;
 let lvl_data;
 let platforms;
 let character;
@@ -39,17 +40,20 @@ let character;
 //preload images and level data
 function preload() {
   lvl_background = loadImage("images/lvl_1_img.jpg");
+  character_img = loadImage("images/f1.png");
   lvl_data = loadStrings("levels/lvl1.txt");
 }
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
+  world.gravity.y = 10;
 
   platforms = new Group();
   platforms.w = 20;
   platforms.h = 20;
   platforms.color = "black";
   platforms.tile = "=";
+  platforms.collider = "static";
 
   new Tiles(
     lvl_data,
@@ -58,9 +62,27 @@ function setup() {
     platforms.w + 1,
     platforms.h + 1
   );
+
+  character = new Sprite();
+  character.img = character_img;
+  character.x = width/2.8;
+  character.y = height/(height/42);
+  character.collider = "d";
 }
 
 function draw() {
+  if (kb.pressing("left")) {
+    character.move(5, "left", 3);
+  }
+  else if (kb.pressing("right")) {
+    character.move(5, "right", 3);
+  }
+  else if (kb.pressing("up")) {
+    character.move(5, "up", 3);
+  }
+  else {
+    character.vel.x = 0;
+  }
   clear();
   image(lvl_background, width/3, 0, width/2.75, height);
 }
