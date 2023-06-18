d0fb5049d0280657d0c4ab71b6857343bf5c3ac9
Eesha <126822458+yhe-c@users.noreply.github.com>
2023-06-18 17:17:09 -0600

    added some extras (sfx and character selection)
    also organized but too tired to optimize certain things so ALL DONE!

--- sketch.js
+++ sketch.js
@@ -5,23 +5,26 @@
 
 //initiating global variables
 let aBtn, bBtn, hBtn, pBtn, rBtn, reBtn, sBtn;
-let ab_page, about_page;
+let about_page;
 let about_button, back_button, start_button;
 let about_button_hov, back_button_hov, start_button_hov;
 let background_music;
-let character, character_img, character_idle, character_walk;
+let bgMusicVol;
+let character, character_img, characterIdle, characterWalkAni;
 let enemies, enemy_img, enemy_movement;
 let enemySpawnTimer;
-let gameEnd, game_over, game_over_menu, game_over_screen;
+let game_end, game_over, gameOverMenu, game_over_screen;
 let gameStatus = 0;
 let lvl_background;
-let lvl_data, levelData;
-let musicSlider;
-let next_lvl, next_lvl_img;
+let lvlData, level_data;
+let nextLvl, next_lvl_img;
 let nextLevel = 1;
-let pause_button, pause_button_hov, pause_menu, pause_screen, paused;
+let pause_button, pause_button_hov, pauseMenu, pause_screen, paused;
 let platforms;
 let showNext = 0;
+let selectBtnF, selectBtnM, selection_bg, select_character;
+let soundJump, soundWard, soundOver;
+let soundOn = true;
 let start_bg_img;
 let state = true;
 let wards, ward_img;
@@ -30,40 +33,48 @@ let wards, ward_img;
 function preload() {
   //AUDIO
   background_music = loadSound("audio/zoldyck_theme.mp3");
+  soundJump = loadSound("audio/jump_sfx.mp3");
+  soundWard = loadSound("audio/ward_sfx.mp3");
+  soundOver = loadSound("audio/gameover_sfx.mp4");
 
-  //BUTTONS - probably could've used and array for each button to half the clutter but I'm too tired
+  //BUTTONS
   about_button = loadImage("images/about_btn.png");
   about_button_hov = loadImage("images/about_btn_h.png");
   back_button_hov = loadImage("images/back_btn_h.png");
   back_button = loadImage("images/back_btn.png");
   pause_button = loadImage("images/pause_btn.png");
   pause_button_hov = loadImage("images/pause_btn_h.png");
+  select_character = [loadImage("images/select_f.png"), loadImage("images/select_f_h.png"), loadImage("images/select_m.png"), loadImage("images/select_m_h.png")];
   start_button = loadImage("images/start_btn.png");
   start_button_hov = loadImage("images/start_btn_h.png");
-  //END OF BUTTONS
 
   //OTHER IMAGES/DATA
   about_page = loadImage("images/about_page.png");
   character_img = loadImage("images/f1.png");
   enemy_img = loadImage("images/enemy0.png");
-  gameEnd = loadImage("images/end.png");
+  game_end = loadImage("images/end.png");
   game_over_screen = loadImage("images/game_over_menu.png");
   game_over = [loadImage("images/gameover1.png"), loadImage("images/gameover2.png"), loadImage("images/gameover3.png"), loadImage("images/gameover4.png"), loadImage("images/gameover5.png"), loadImage("images/gameover6.png")];
-  levelData = [loadStrings("levels/lvl1.txt"), loadStrings("levels/lvl2.txt"), loadStrings("levels/lvl3.txt"), loadStrings("levels/lvl4.txt"), loadStrings("levels/lvl5.txt"), loadStrings("levels/lvl6.txt"), ];
+  level_data = [loadStrings("levels/lvl1.txt"), loadStrings("levels/lvl2.txt"), loadStrings("levels/lvl3.txt"), loadStrings("levels/lvl4.txt"), loadStrings("levels/lvl5.txt"), loadStrings("levels/lvl6.txt"), ];
   lvl_background = loadImage(`images/lvl_${nextLevel}_img.jpg`);
   next_lvl_img = loadImage("images/next_lvl.png");
   pause_screen = loadImage("images/pause_menu.png");
+  selection_bg = loadImage("images/selection_bg.png");
   start_bg_img = loadImage("images/start_bg.png");
   ward_img = loadImage("images/ward.png");
 }
 
-//setup canvas, groups, tiles, and sprites 
+//setup audio, canvas, groups, tiles, and sprites 
 function setup() {
+  //audio
+  bgMusicVol = createSlider(0, 1, 0.25, 0.01);
+  bgMusicVol.position(1055, 15);
+  bgMusicVol.style("width", "100px");
   background_music.loop();
-  background_music.setVolume();
-  musicSlider = createSlider(0, 1, 0.25, 0.01);
-  musicSlider.position(1055, 15);
-  musicSlider.style("width", "100px");
+  soundJump.setVolume(0.01);
+  soundWard.setVolume(0.25);
+  soundOver.setVolume(0.2);
+  //canvas
   new Canvas(start_bg_img.w, start_bg_img.h + 45);
   world.gravity.y = 10;
   image(start_bg_img, 0, 0, width, height);
@@ -84,6 +95,22 @@ function setup() {
   bBtn.y = 30;
   bBtn.collider = "s";
   bBtn.visible = false;
+
+  selectBtnF = new Group(); //select female character button
+  imageMode = CENTER;
+  selectBtnF.x = width/4;
+  selectBtnF.y = height/2;
+  selectBtnF.collider = "s";
+  selectBtnF.scale = 2;
+  selectBtnF.remove();
+
+  selectBtnM = new Group(); //select male character button
+  imageMode = CENTER;
+  selectBtnM.x = width/1.4;
+  selectBtnM.y = height/2;
+  selectBtnM.collider = "s";
+  selectBtnM.scale = 2;
+  selectBtnM.remove();
   
   pBtn = new Group();  //pause button
   imageMode = CENTER;
@@ -102,12 +129,12 @@ function setup() {
   //END OF BUTTONS
 
   //TILES
-  next_lvl = new Group();  //teleport to next level
-  next_lvl.scale = 1.5;
-  next_lvl.tile = "$";
-  next_lvl.img = next_lvl_img;
-  next_lvl.collider = "s";
-  next_lvl.visible = false;
+  nextLvl = new Group();  //teleport to next level
+  nextLvl.scale = 1.5;
+  nextLvl.tile = "$";
+  nextLvl.img = next_lvl_img;
+  nextLvl.collider = "s";
+  nextLvl.visible = false;
 
   platforms = new Group();  //platforms and walls group
   platforms.w = width/28;
@@ -126,16 +153,15 @@ function setup() {
   wards.collider = "s";
   wards.visible = false;
   
-  lvl_data = levelData[0];  //loading first level
+  lvlData = level_data[0];  //loading first level
   new Tiles(
-    lvl_data,
+    lvlData,
     0,
     0,
     platforms.w,
     platforms.h 
   );
-  //END OF TILES
-
+  
   //enemy group
   enemies = new Group();
   enemies.img = enemy_img;
@@ -147,41 +173,31 @@ function setup() {
   enemies.ani.frameDelay = 10;
   enemySpawnTimer = new Timer(10000 - 1000 * nextLevel);
   enemies.remove();
-
-  //character sprite     
-  character = new Sprite();
-  character.img = character_img;
-  character.collider = "d";
-  character.friction = 0;
-  character.rotationLock = true;
-  character.overlaps(wards, collect);
-  character.overlaps(enemies, game);
-  character.overlaps(next_lvl, updateLvl);
-  character_idle = loadAni("images/f1.png");
-  character_idle.frameDelay = 10;
-  character.addAni("idle", character_idle);
-  character_walk = loadAni("images/f1.png", "images/f2.png", "images/f3.png");
-  character_walk.frameDelay = 8;
-  character.addAni("walk", character_walk);
-  character.visible = false;
   
   //pause menu
-  pause_menu = new Group();
-  pause_menu.img = pause_screen;
-  pause_menu.x = width/2;
-  pause_menu.y = height/2;
-  pause_menu.scale = 2;
-  pause_menu.collider = "s";
-  pause_menu.remove();
+  pauseMenu = new Group();
+  pauseMenu.img = pause_screen;
+  pauseMenu.x = width/2;
+  pauseMenu.y = height/2;
+  pauseMenu.scale = 2;
+  pauseMenu.collider = "s";
+  pauseMenu.remove();
 
   //game over menu
-  game_over_menu = new Group();
-  game_over_menu.img = game_over_screen;
-  game_over_menu.x = width/2;
-  game_over_menu.y = height/1.2;
-  game_over_menu.scale = 2;
-  game_over_menu.collider = "s";
-  game_over_menu.remove();
+  gameOverMenu = new Group();
+  gameOverMenu.img = game_over_screen;
+  gameOverMenu.x = width/2;
+  gameOverMenu.y = height/1.2;
+  gameOverMenu.scale = 2;
+  gameOverMenu.collider = "s";
+  gameOverMenu.remove();
+}
+
+//function to collect the wards
+function collect(character, ward) {
+  soundWard.play();
+  ward.remove();
+  showNext++;
 }
 
 //function to spwan enemies
@@ -202,16 +218,10 @@ async function enemySequence(i) {
   enemySequence();
 }
 
-//function to collect the wards
-function collect(character, ward) {
-  ward.remove();
-  showNext++;
-}
-
 //function to update level when completed
 function updateLvl() {
   if (nextLevel < 6) {
-    lvl_data = levelData[nextLevel];
+    lvlData = level_data[nextLevel];
     nextLevel++;
     lvl_background = loadImage(`images/lvl_${nextLevel}_img.jpg`);
     reloadLvl();
@@ -228,14 +238,14 @@ function reloadLvl() {
   enemySpawnTimer.start();
   wards.remove();
   enemies.remove();
-  next_lvl.remove();
+  nextLvl.remove();
   platforms.remove();
   character.x = 35;
   character.y = 45;
   if (nextLevel === 4) { //for level 4 because the background is too dark
     platforms.color = "grey";
     new Tiles(
-      lvl_data,
+      lvlData,
       0,
       0,
       platforms.w,
@@ -245,15 +255,43 @@ function reloadLvl() {
   else { //for every other level
     platforms.color = "black";
     new Tiles(
-      lvl_data,
+      lvlData,
       0,
       0,
       platforms.w,
       platforms.h
     );
   }
-  next_lvl.visible = false;
+  nextLvl.visible = false;
   showNext = 0;
+}
+
+//function that shows the start menu
+function startMenu(){
+  image(start_bg_img, 0, 0, width, height);
+  pBtn.visible = false;
+  wards.visible = false;
+  platforms.visible = false;
+  nextLevel.visible = false;
+  enemies.visible = false;
+  aBtn.img = about_button;
+  sBtn.img = start_button;
+  new aBtn.Sprite();
+  new sBtn.Sprite();
+  if (sBtn.mouse.hovering()) {
+    sBtn.img = start_button_hov;
+  }
+  if (sBtn.mouse.pressed()) {
+    gameStatus = 0.75;
+  }
+  if (aBtn.mouse.hovering()) {
+    aBtn.img = about_button_hov;
+  }
+  if (aBtn.mouse.pressed()) {
+    gameStatus = 0.5;
+    new bBtn.Sprite();
+    aBtn.visible = false;
+  }
 }
 
 //function that shows the about page
@@ -273,68 +311,65 @@ function aboutPage() {
   }
 }
 
-//function that shows the start menu
-function startMenu(){
-  image(start_bg_img, 0, 0, width, height);
-  pBtn.visible = false;
-  wards.visible = false;
-  platforms.visible = false;
-  character.visible = false;
-  nextLevel.visible = false;
-  enemies.visible = false;
-  aBtn.img = about_button;
-  sBtn.img = start_button;
-  new aBtn.Sprite();
-  new sBtn.Sprite();
-  if (sBtn.mouse.hovering()) {
-    sBtn.img = start_button_hov;
+function selectCharacter() {
+  sBtn.remove();
+  aBtn.remove();
+  image(selection_bg, 0, 0, width, height);
+  bBtn.img = back_button;
+  new bBtn.Sprite();
+  bBtn.visible = true;
+  selectBtnF.img = select_character[0];
+  new selectBtnF.Sprite();
+  selectBtnM.img = select_character[2];
+  new selectBtnM.Sprite();
+  if (selectBtnF.mouse.hovering()) {
+    selectBtnF.img = select_character[1];
   }
-  if (sBtn.mouse.pressed()) {
-    enemySpawnTimer.start();
-    new pBtn.Sprite();
-    reloadLvl();
-    gameStatus = 1;
-    sBtn.remove();
-    aBtn.remove();
-    bBtn.remove();
+  if (selectBtnF.mouse.pressed()) {
+    startIt("f");
   }
-  if (aBtn.mouse.hovering()) {
-    aBtn.img = about_button_hov;
+  if (selectBtnM.mouse.hovering()) {
+    selectBtnM.img = select_character[3];
   }
-  if (aBtn.mouse.pressed()) {
-    gameStatus = 0.5;
-    new bBtn.Sprite();
-    aBtn.visible = false;
+  if (selectBtnM.mouse.pressed()) {
+    startIt("m");
+  }
+  if (bBtn.mouse.hovering()) {
+    bBtn.img = back_button_hov;
+  }
+  if (bBtn.mouse.pressed()) {
+    selectBtnF.remove();
+    selectBtnM.remove();
+    bBtn.visible = false;
+    sBtn.visible = true;
+    aBtn.visible = true;
+    gameStatus = 0;
   }
 }
 
-//main draw loop checking for game status
-function draw() {
-  let vol = musicSlider.value();
-  background_music.setVolume(vol);
-  if (gameStatus === 0) { //game status for showing the start menu
-    startMenu();
-  }
-  else if (gameStatus === 0.5) { //game status for showing the about page
-    aboutPage();
-  }
-  else if (gameStatus === 1) { //game status for starting the game
-    clear();
-    startGame();
-  }
-  else if (gameStatus === -0.5) { //game status for failure of completion of a level
-    gameOver();
-  }
-  else if (gameStatus === -1) { //game status for completion of the game
-    image(gameEnd, 0, 0, width, height);
-    platforms.remove();
-    wards.remove();
-    enemies.remove();
-    next_lvl.remove();
-    pBtn.remove();
-    character.visible = false;
-  }
-  //translate(random(-1,1),random(-1,1)); //shaking effect
+function startIt(char) {
+  bBtn.remove();
+  selectBtnF.remove();
+  selectBtnM.remove();
+  enemySpawnTimer.start();
+  new pBtn.Sprite();
+  character = new Sprite();
+  character.img = character_img;
+  character.collider = "d";
+  character.friction = 0;
+  character.rotationLock = true;
+  character.overlaps(wards, collect);
+  character.overlaps(enemies, game);
+  character.overlaps(nextLvl, updateLvl);
+  characterIdle = loadAni(`images/${char}1.png`);
+  characterIdle.frameDelay = 10;
+  character.addAni("idle", characterIdle);
+  characterWalkAni = loadAni(`images/${char}1.png`, `images/${char}2.png`, `images/${char}3.png`);
+  characterWalkAni.frameDelay = 5;
+  character.addAni("walk", characterWalkAni);
+  reloadLvl();
+  gameStatus = 1;
+  
 }
 
 //function that starts and runs most of the game
@@ -354,16 +389,17 @@ function startGame() {
       pBtn.img = pause_button_hov;
     }
     if (pBtn.mouse.pressed()) {
-      new pause_menu.Sprite();
+      new pauseMenu.Sprite();
       paused = true;
       noLoop();
     }
     if (showNext === 3) { //show teleport for the next level after collecting all 3 wards
-      next_lvl.visible = true;
+      nextLvl.visible = true;
     }
     if (kb.presses("up") || kb.presses("w")) { //jumping
       if (character.colliding(platforms)) {
         character.vel.y = -6.75;
+        soundJump.play();
       }
     }
     if (kb.pressing("left") || kb.presses("a")) { //left
@@ -383,39 +419,86 @@ function startGame() {
   }
 }
 
+//main draw loop checking for game status
+function draw() {
+  if (soundOn) {
+    soundJump.setVolume(0.01);
+    soundWard.setVolume(0.25);
+    soundOver.setVolume(0.2);
+  }
+  if (!soundOn) {
+    soundJump.setVolume(0);
+    soundWard.setVolume(0);
+    soundOver.setVolume(0);
+  }
+  if (gameStatus === 0) { //game status for showing the start menu
+    startMenu();
+  }
+  else if (gameStatus === 0.5) { //game status for showing the about page
+    aboutPage();
+  }
+  else if (gameStatus === 0.75) {
+    selectCharacter();
+  }
+  else if (gameStatus === 1) { //game status for starting the game
+    clear();
+    startGame();
+  }
+  else if (gameStatus === -0.5) { //game status for failure of completion of a level
+    gameOver();
+  }
+  else if (gameStatus === -1) { //game status for completion of the game
+    image(game_end, 0, 0, width, height);
+    platforms.remove();
+    wards.remove();
+    enemies.remove();
+    nextLvl.remove();
+    pBtn.remove();
+    character.visible = false;
+  }
+  let vol = bgMusicVol.value();
+  background_music.setVolume(vol);
+  //translate(random(-1,1),random(-1,1)); //shaking effect
+}
+
 function keyPressed() {
   if (keyCode === BACKSPACE) {
     if (paused) {
-      pause_menu.remove();
+      pauseMenu.remove();
       paused = false;
       reloadLvl();
       loop();
     }
     else if (gameStatus === -0.5) {
-      game_over_menu.remove();
+      gameOverMenu.remove();
       gameStatus = 1;
       reloadLvl();
       loop();
     } 
   }
   if (keyCode === ENTER) {
-    pause_menu.remove();
+    pauseMenu.remove();
     loop();
   }
   if (keyCode === ESCAPE) {
     if (paused) {
+      character.visible = false;
       enemies.remove();
-      pause_menu.remove();
+      pauseMenu.remove();
       paused = false;
       gameStatus = 0;
       loop();
     }
     else if (gameStatus === -0.5) {
+      character.visible = false;
       enemies.remove();
-      game_over_menu.remove();
+      gameOverMenu.remove();
       gameStatus = 0;
       loop();
     } 
+  }
+  if (keyCode === SHIFT) {
+    soundOn = !soundOn;
   }
 }
 
@@ -425,12 +508,13 @@ function game() {
 
 //function for failure of completion of the level
 function gameOver() {
+  soundOver.play();
   let bgNum = Math.floor(random(0, 6));
   image(game_over[bgNum], 0, 0, width, height);
-  new game_over_menu.Sprite();
+  new gameOverMenu.Sprite();
   wards.remove();
   enemies.remove();
-  next_lvl.remove();
+  nextLvl.remove();
   platforms.remove();
   pBtn.visible = false;
   character.visible = false;
